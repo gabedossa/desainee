7a20b44544e4d916148ab9298a49a837b155bd86
gabedossa <gabrielvinces2@gmail.com>
2024-03-31 00:04:55 -0300

    project builded

---
+++ Backend/src/main/java/com/gabrieldossa/desainee/DesaineeApplication.java
@@ -0,0 +1,13 @@
+package com.gabrieldossa.desainee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesaineeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesaineeApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/gabrieldossa/desainee/DesaineeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrieldossa.desainee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesaineeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
